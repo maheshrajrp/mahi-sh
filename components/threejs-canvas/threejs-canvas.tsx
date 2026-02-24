3d82f639f3c66bc4fd7cd14c2c0375ae5be22397
maheshrajrp <maheshrajrp@gmail.com>
2026-02-25 01:46:31 +0530

    removed content

--- components/threejs-canvas/threejs-canvas.tsx
+++ components/threejs-canvas/threejs-canvas.tsx
@@ -1,21 +1,18 @@
 import Asteroids from '@/components/asteroids/asteroids'
 import { ProjectBoards } from '@/components/project-boards/project-boards'
 import { Stars } from '@/components/stars/stars'
-import { CameraControls, ScrollControls, Stats } from '@react-three/drei'
+import { CameraControls, Stats } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 
 export const ThreeJSCanvas = ({ children }: { children?: React.ReactNode }) => {
     return <>
         <Canvas gl={{ stencil: true }} >
-            <ScrollControls pages={4} damping={0.25}>
-                <ambientLight intensity={1} />
-                <CameraControls />
-                <Stats />
-                <Stars />
-                <ProjectBoards />
-                <Asteroids />
-                {children}
-            </ScrollControls>
+            <ambientLight intensity={1} />
+            <CameraControls />
+            <Stats />
+            <Stars />
+            <ProjectBoards />
+            <Asteroids />
         </Canvas>
     </>
 }
